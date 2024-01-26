816f31214195d240cf423946c343839fc6a41298
Peeter Randria <peteryan.randria@gmail.com>
2024-01-27 02:11:08 +0300

    dev/connection
    Connection_established_successfully

--- DBConnect.cs
+++ DBConnect.cs
@@ -7,22 +7,31 @@ namespace connect
 
     public class DBConnect
     {
+        private string test;
+
+        public string Test { get; set; }
 
         public DBConnect()
         {
-            String connectionString = "Host=localhost;Username=pharmacie;Password=pharmacie;Database=pharmacie";
+            Init_Connection();
+        }
+
+        private void Init_Connection()
+        {
+            String connectionString = "Host=localhost;Port=5432;Database=pharmacie;Username=pharmacie;Password=pharmacie;";
 
             using var connection = new NpgsqlConnection(connectionString);
 
             try
             {
                 connection.Open();
-
+                Test = "Connected to PostgreSQL!";
                 Console.WriteLine("Connected to PostgreSQL!");
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
+                Test = $"Error: {ex.Message}";
             }
             finally
             {
@@ -30,5 +39,4 @@ namespace connect
             }
         }
     }
-
 }
