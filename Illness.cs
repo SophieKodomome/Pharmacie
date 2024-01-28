00efc39ace15a60fcdbe4c82c3301b11c90a39b3
Peeter Randria <peteryan.randria@gmail.com>
2024-01-28 15:42:35 +0300

    update/Illness

--- Illness.cs
+++ Illness.cs
@@ -6,8 +6,40 @@ namespace Medical
     {
 
         private List<Symptom> listSymptoms;
+
+        private int maxPain;
+
+        private int minPain;
         public List<Symptom> ListSymptoms { get; set; }
 
+        public int MaxPain{            
+            get { return maxPain; }
+            set
+            {
+                if (value <= 10)
+                {
+                    maxPain = value;
+                }
+                else
+                {
+                    throw new Exception("max pain can't go higher than 10");
+                }
+            }
+        }
+        public int MinPain{            
+            get { return minPain; }
+            set
+            {
+                if (value > 0)
+                {
+                    minPain = value;
+                }
+                else
+                {
+                    throw new Exception("max pain can't go higher than 10");
+                }
+            }
+        }
         public Illness addListSymptoms(List<Symptom> s)
         {
             ListSymptoms = s;
