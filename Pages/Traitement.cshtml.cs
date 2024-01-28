ae9d914e8a3aa025f11646444b47fad0ed295f07
Peeter Randria <peteryan.randria@gmail.com>
2024-01-28 10:38:53 +0300

    dev/conversion_to_int_array

--- Pages/Traitement.cshtml.cs
+++ Pages/Traitement.cshtml.cs
@@ -9,6 +9,8 @@ namespace Pharmacie.Pages;
 public class TraitementModel : PageModel
 {
     public string concatSymptomValues {get;set;}
+
+    public int[] symptomValues;
     public DBConnect connect;
     public List<Symptom> ListSymptoms { get; set; }
     private readonly ILogger<TraitementModel> _logger;
@@ -23,12 +25,18 @@ public class TraitementModel : PageModel
     if (TempData["severity"] != null)
     {
         concatSymptomValues = TempData["severity"].ToString();
-        Console.WriteLine(concatSymptomValues);
+        char[] delimiter = new char[] { '-' };
+        symptomValues=Array.ConvertAll(concatSymptomValues.Split(delimiter),int.Parse);
+
+        for(int i=0;i<symptomValues.Length;i++){
+            Console.WriteLine(symptomValues[i]);
+        }
     }
     else
     {
         // Handle the case where TempData["severity"] is null
         Console.WriteLine("TempData['severity'] is null");
+
     }
     }
 }
