c05f2dd5271097d5202343902ef7e3456970472a
Peeter Randria <peteryan.randria@gmail.com>
2024-01-28 17:07:11 +0300

    dev/update_illness_and_symptom

--- Illness.cs
+++ Illness.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using Microsoft.VisualBasic;
+using Npgsql;
 
 namespace Medical
 {
@@ -7,39 +9,9 @@ namespace Medical
 
         private List<Symptom> listSymptoms;
 
-        private int maxPain;
-
-        private int minPain;
         public List<Symptom> ListSymptoms { get; set; }
 
-        public int MaxPain{            
-            get { return maxPain; }
-            set
-            {
-                if (value <= 10)
-                {
-                    maxPain = value;
-                }
-                else
-                {
-                    throw new Exception("max pain can't go higher than 10");
-                }
-            }
-        }
-        public int MinPain{            
-            get { return minPain; }
-            set
-            {
-                if (value > 0)
-                {
-                    minPain = value;
-                }
-                else
-                {
-                    throw new Exception("max pain can't go higher than 10");
-                }
-            }
-        }
+
         public Illness addListSymptoms(List<Symptom> s)
         {
             ListSymptoms = s;
@@ -51,8 +23,34 @@ namespace Medical
             ListSymptoms = s;
         }
 
-        public Illness(int i, string n) : base(i, n){}
+        public Illness(int i, string n) : base(i, n) { }
 
-        public Illness(){}
+        public Illness() { }
+
+        public List<Illness> GetIllnessesFromDB(NpgsqlConnection connection)
+        {
+            //yes it is a bit long
+            List<Illness> listIllness = new List<Illness>();
+            //List<Symptom> listSymp=new List<Symptom>();
+            int i=0;
+
+            connection.Open();
+            using(var command=new NpgsqlCommand("select distinct nom_illness,id_illness from v_diagnoses order by id_illness;",connection))
+            {
+                using(var reader=command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        Illness ill=new Illness();
+                        ill.addListSymptoms(new Symptom().getSymptomsForIllness(connection,i))
+                            .addName(reader.GetString(0))
+                            .addId(reader.GetInt32(1));
+
+                        i++;
+                    }
+                }
+            }
+            return listIllness;
+        }
     }
 }
--- Symptom.cs
+++ Symptom.cs
@@ -7,6 +7,9 @@ namespace Medical
 {
     public class Symptom : Diagnose
     {
+        private int maxPain;
+
+        private int minPain;
         private int severity;
 
         public int Severity
@@ -31,6 +34,49 @@ namespace Medical
             return this;
         }
 
+        public int MaxPain
+        {
+            get { return maxPain; }
+            set
+            {
+                if (value <= 10)
+                {
+                    maxPain = value;
+                }
+                else
+                {
+                    throw new Exception("max pain can't go higher than 10");
+                }
+            }
+        }
+        public int MinPain
+        {
+            get { return minPain; }
+            set
+            {
+                if (value > 0)
+                {
+                    minPain = value;
+                }
+                else
+                {
+                    throw new Exception("min pain can't go lower than 0");
+                }
+            }
+        }
+
+        public Symptom addMaxPain(int m)
+        {
+            MaxPain = m;
+            return this;
+        }
+
+        public Symptom addMinPain(int m)
+        {
+            MinPain = m;
+            return this;
+        }
+
         public Symptom() { }
         public Symptom(int i, string n, int s) : base(i, n)
         {
@@ -51,6 +97,33 @@ namespace Medical
                     {
                         Symptom s = new Symptom();
                         s.addId(reader.GetInt32(0)).addName(reader.GetString(1));
+                        listSymptoms.Add(s);
+                    }
+                }
+            }
+
+            connection.Close();
+            return listSymptoms;
+        }
+
+        public List<Symptom> getSymptomsForIllness(NpgsqlConnection connection,int id)
+        {
+            List<Symptom> listSymptoms = new List<Symptom>();
+
+            connection.Open();
+
+            using (var command = new NpgsqlCommand("SELECT * from v_diagnoses where id_illness="+id+";", connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Symptom s = new Symptom();
+                        s
+                            .addMaxPain(reader.GetInt32(5))
+                            .addMinPain(reader.GetInt32(6))
+                            .addName(reader.GetString(2))
+                            .addId(reader.GetInt32(4));
                         listSymptoms.Add(s);
                     }
                 }
